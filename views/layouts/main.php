6058dd9141ad3f3150ddb3a2e9c128f925849d93
Vladislav Shilov <vlad.shilov03@mail.ru>
2023-05-10 22:38:31 +0700

    start ep1

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,98 +1,97 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
 <head>
-   <meta charset="UTF-8">
-   <meta name="viewport"
-         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Главная страница</title>
-   <style>
-	body {
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+	<div class="navbar">
+		<div class="logo">
+			<div class="img_logo">
+				<img src="" alt="логотип">
+			</div>
+			<p>NAME</p>
+			<div class="profile">профиль</div>
+		</div>
+
+		<div class="tape">
+			<a href="#">новости</a>
+			<a href="#">сообщения</a>
+			<a href="#">друзья</a>
+			<a href="#">заметки</a>
+			<a href="#">что-то</a>
+			<a href="#">о нас</a>
+		</div>
+	</div>
+
+	<main>
+		<div class="block"></div>
+</main>
+<style>
+		*{
 	margin: 0;
 	padding: 0;
+	font-size: 24px;
 }
-.header{
-    margin-top: -20px;
-	height: 100px;
-	background-color: #B7F0B6;
+
+.navbar{
+	padding-top: 30px;
+	background-color: #001930;
 }
-.nav{
+
+.logo >p{
+	color: #fff;
+}
+
+.logo{
+	width: 1700px;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
-	margin-left: 40px;
-	margin-right: 40px;
-    padding-top: 40px;
-    
+	margin: 0 auto;
 }
-.logo >a {
-	font-size: 32px;
-    text-decoration: none;
-	color: #000;
-	padding: 20px;
+
+.img_logo{
+	background-color: #1D82DF;
+	border-radius: 40px;
+	color: #fff;
+	padding: 15px;
 }
-.link >a {
-	text-decoration: none;
-	color: #000;
-	padding: 20px;
-	font-size: 24px;
+
+.profile{
+	background-color: #1D82DF;
+	border-radius: 40px;
+	color: #fff;
+	padding: 15px;
 }
-.link >a:hover{
-	color: pink;
-}
-.center{
-	margin-top: 100px;
+
+.tape{
+	width: 1700px;
 	display: flex;
-	justify-content: center;
+	align-items: center;
+	justify-content: space-between;
+	margin: 0 auto;
+	padding-top: 40px;
+	padding-bottom: 20px;
 }
+
+.tape >a{
+	background-color: #fff;
+	border-radius: 25px;
+	padding: 10px 30px 10px 30px;
+	text-decoration: none;
+}
+
 .block{
 	background-color: #000;
-	width: 824px;
-	height: 726px;
+	height: 700px;
+	width: 1500px;
+	margin: 0 auto;
+	margin-top: 40px;
 }
-</style>
-</head>
-<body>
-	<div class="header">
-		<div class="nav">
-			<div class="logo">
-                <a href="<?= app()->route->getUrl('/hello')?>">Телефонный узел связи ТОМСК</a>
-			</div>
-			<div class="link">
-			<?php
-        if (!app()->auth::check()):
-            ?>
-            <a href="<?= app()->route->getUrl('/signup')?>">SIGN UP</a>
-            <a href="<?= app()->route->getUrl('/login')?>">LOGIN</a>	 
-		<?php
-        else:
-            ?>
-			<?php
-if (app()->auth::User()->id_role === 1 || 2):
-    ?>
-			<a href="<?= app()->route->getUrl('/add_user')?>">ADD_ABONENT</a>
-			<a href="<?= app()->route->getUrl('/profile')?>">ABONENT</a>
-			<?php
-        endif;
-        ?>
-			<a href="<?= app()->route->getUrl('/room')?>">ROOM</a>
-			<a href="<?= app()->route->getUrl('/subdivision')?>">SUBDIVISION</a>	 
-			<a href="<?= app()->route->getUrl('/logout')?>">LOGOUT(<?= app()->auth::User()->login ?>)</a>
-		<?php
-        endif;
-        ?>
-			</div>
-		</div>
-	</div>
-	<main>
-		<?= $content ?? '' ?>
-		
-	</main>
+	</style>
 </body>
 </html>
-
-	
-
-
-
-
